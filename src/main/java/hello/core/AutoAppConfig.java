e876e43c899e69b400bc584c038ffd7c6b743249
awesomeo184 <awesomeo184@gmail.com>
2021-01-27 13:45:59 +0900

    39-40강 컴포넌트 탐색과 필터

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,9 +6,12 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-    // 예제 AppConfig와 충돌 방지
-    excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
-        classes =Configuration.class)
+    // 등록된 패키지부터 탐색. 디폴트는 componentScan이 등록된 클래스가 속한 패키지 및 하위 패키지
+    basePackages = "hello.core",
+//    basePackageClasses = AutoAppConfig.class,
+//     예제 AppConfig와 충돌 방지
+    excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION,
+    classes = Configuration.class)
 )
 public class AutoAppConfig {
 
